2422056e7d9263d74d4c7e1047e65d6ba433fcc0
Brian K Kemboi <bkemboi590@gmail.com>
2022-12-27 07:58:38 +0300

    another files

---
+++ compnents/Person.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Person({person}) {
+  return (
+      <div>
+          <h2>
+ I am {person.name}, I am {person.age} years old. I know {person.skill}
+        </h2>   
+    </div>
+  )
+}
+
+export default Person 
+//this is the child component and linked to the parent OrderList.js
+//used to render out html
---
+++ compnents/Snippets.js
@@ -0,0 +1,3 @@
+// rce ====>react class componet snipet
+// rfce ====> react functional component snippet
+// rconst ====> react constructor 
