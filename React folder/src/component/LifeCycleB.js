f60c76d13ee54b5f64c15019a509de4da4414d84
Brian K Kemboi <bkemboi590@gmail.com>
2023-01-01 16:19:13 +0300

    life cyles methods

--- React folder/src/component/LifeCycleB.js
+++ React folder/src/component/LifeCycleB.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class LifeCycleB extends Component {
      constructor(props) {
        super(props)
-     
+      
        this.state = {
           name: 'Kemboi'
          }
